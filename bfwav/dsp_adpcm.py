2eccfa0dd3c60c12867ad12eccd84a8bab15e078
Aarni Koskela <akx@iki.fi>
2018-03-15 16:49:52 +0200

    Speed up dsp_adpcm conversion slightly

--- bfwav/dsp_adpcm.py
+++ bfwav/dsp_adpcm.py
@@ -26,6 +26,8 @@ def decode_adpcm(src_data, initial_hist1, initial_hist2, num_samples, coefs):
     samples = []
     data_sio = io.BytesIO(src_data)
     read_byte = lambda: data_sio.read(1)[0]
+    BYTES_PER_FRAME = tuple(range(7))
+    SAMPLES_PER_BYTE = tuple(range(2))
 
     while len(samples) < num_samples:
         # Each frame, we need to read the header byte and use it to set the scale and coefficient values:
@@ -35,9 +37,9 @@ def decode_adpcm(src_data, initial_hist1, initial_hist2, num_samples, coefs):
         coef1 = coefs[coef_index * 2 + 0]
         coef2 = coefs[coef_index * 2 + 1]
 
-        for b in range(7):  # 7 bytes per frame
+        for b in BYTES_PER_FRAME:  # 7 bytes per frame
             byte = read_byte()
-            for s in range(2):  # 2 samples per byte
+            for s in SAMPLES_PER_BYTE:  # 2 samples per byte
                 adpcm_nibble = (get_high_nibble(byte) if s == 0 else get_low_nibble(byte))
                 sample = clamp(((adpcm_nibble * scale) << 11) + 1024 + ((coef1 * hist1) + (coef2 * hist2)) >> 11)
                 hist2 = hist1
